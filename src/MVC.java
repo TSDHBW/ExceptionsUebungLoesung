34e36c82ec3044d9cc41267c02a4f9ea957639fa
timo_macbookair <timoj.schmelzle@gmail.com>
2022-04-20 14:47:34 +0200

    Erstellung

--- src/MVC.java
+++ src/MVC.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MVC {
@@ -16,14 +17,45 @@ public class MVC {
             System.out.println("1: Produkt aufnehmen");
             System.out.println("2: Produkt anzeigen");
             System.out.println("3: Beenden");
-            int auswahl = scan.nextInt();
+            // Falls kein Int eingegeben wird, entsteht eine InputMismachtException, die abzufangen ist
+            int auswahl=0;
+            boolean auswahlErfolg = false;
+            // Schleife läuft mind. 1x mit dem Ziel über einen Try-Catch Konstrukt fehlerhafte Eingaben z.B. von Buchstaben
+            // abzufangen. die Schleife läuft so lange bis eine korrekte Eingabe erfolgt ist.
+            do {
+                try {
+                    auswahl = scan.nextInt();
+                    auswahlErfolg = true;
+                } catch (InputMismatchException e){
+                    //scan.next sorgt dafür, dass die fehlerhafte Eingabe aus dem Puffer gelöscht wird.
+                    System.out.println(scan.next() + " ist Keine gültige Auswahl. Bitte 1,2 oder 3 eingeben");
+                }
+            } while (!auswahlErfolg);
 
             if (auswahl == 1){
 
                 System.out.println("Gib den Namen des Produkts ein");
+                // Je nach Art der verwalteten Produktpreise kann es Sinn machen die Eingabe des Namens gesondert zu
+                // behandeln z.B. um eine Eingabe von Zahlen zu verhindern
                 String name = scan.next();
                 System.out.println("Gib den Preis des Produkts ein");
-                double preis = scan.nextDouble();
+                // Falls kein Double oder implizit konvertierbarer Wert (z.B: int) eingegeben wird, entsteht eine
+                // InputMismatchException, die abzufangen ist
+                double preis =0.0;
+                auswahlErfolg = false;
+                // Schleife läuft mind. 1x mit dem Ziel über einen Try-Catch Konstrukt fehlerhafte Eingaben z.B. von Buchstaben
+                // abzufangen. die Schleife läuft so lange bis eine korrekte Eingabe erfolgt ist.
+                do {
+                    try {
+                        preis = scan.nextDouble();
+                        auswahlErfolg = true;
+                    } catch (InputMismatchException e){
+                        //scan.next sorgt dafür, dass die fehlerhafte Eingabe aus dem Puffer gelöscht wird.
+                        //Prüfung auf negative Werte kann inhaltlich zusätzlich ergänzt werden.
+                        System.out.println(scan.next() + " ist Keine gültige Auswahl. Bitte eine Zahl eingeben");
+                    }
+                } while (!auswahlErfolg);
+
                 Produkt produkt = new Produkt(name, preis);
                 preise.nehmeProduktAuf(produkt);
                 System.out.println("Produkt erfolgreich aufgenommen");
@@ -31,14 +63,29 @@ public class MVC {
             } else if (auswahl == 2){
 
                 System.out.println("Gib den Namen des Produkts ein");
+                // Prüfen warum NullPointerException -> ggf. durch .equals?
                 String name = scan.next();
-                System.out.println(preise.zeigeProdukt(name).name);
-                System.out.println(preise.zeigeProdukt(name).preis);
+                // zeigeProdukt(name) kann ein null zurückgeben, falls kein Produkt gefunden wird. Ohne Behandlung
+                // entsteht bei den Ausgaben eine NUllPointerException, da "null" kein Namen oder Preis hat.
+                if (preise.zeigeProdukt(name) != null){
 
+                    System.out.println(preise.zeigeProdukt(name).name);
+                    System.out.println(preise.zeigeProdukt(name).preis);
+
+                } else {
+                    System.out.println("Produkt nicht gefunden. Bitte Name prüfen");
+                }
             }
             if (auswahl == 3){
                 exit = true;
             }
+            // Ergänzung für ungültige Auswahl.
+            if (auswahl != 1 & auswahl !=2 & auswahl !=3 ){
+
+                System.out.println("Ungültige Auswahl");
+
+            }
         }
+        scan.close();
     }
 }
--- src/Preisverwaltung.java
+++ src/Preisverwaltung.java
@@ -12,26 +12,27 @@ public class Preisverwaltung {
 
     public void nehmeProduktAuf (Produkt produkt){
 
-        preise[index] = produkt;
-        index++;
+        // Bringt das Risiko von ArrayOutOfBoundExceptions, da der Index nicht an die Länge des Arrays gekoppelt ist
+        // und daher versucht werden kann ein Produkt in das Array zu legen, obwohl es bereits voll ist
+        if (index < preise.length){
+            preise[index] = produkt;
+            index++;
+        } else {
+            System.out.println("Es kann kein neues Produkt aufgenommen werden");
+        }
+        // Für eine optimale Benutzerführung wäre die Prüfung der Aufnahme vor Eingabe des Namens und des Preises besser
 
     }
 
     public Produkt zeigeProdukt (String name){
 
         for (int i = 0; i < preise.length; i++){
-
-            if (preise[i].name.equals(name)){
+            if (preise[i] != null && preise[i].name.equals(name)){
 
                 return preise[i];
 
             }
-
         }
         return null;
-
     }
-
-
-
 }
